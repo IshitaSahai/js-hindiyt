34bc5b64ab4b743d8df49efff89714ab13974beb
Ishita Sahai <ishitasahai0999@gmail.com>
2025-06-18 17:50:10 +0000

    conversion operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -4,7 +4,7 @@ console.log(typeof score)
 console.log(typeof (score))
 // string to number
 let intv=Number(score) //type conversion from string to number
-console.log(typeof intv)
+console.log(typeof intv) 
 // console.log()
 // "33"-> 33
 // "33sfj"->NaN
@@ -21,3 +21,32 @@ let strno= String(someno)
 console.log(strno)
 console.log(typeof strno)
 
+// OPERATIONS:- 
+let val=3
+let neg=-val
+// console.log(neg)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)
+
+
+let s1="efdb"
+let s2="defd"
+let s3=s1+s2
+console.log("1"+2)
+console.log(1+"2")
+console.log(2+2+"2")
+console.log("2"+2+2)
+
+
+//console.log((3+4)*5%4)
+console.log(+true)
+console.log(+"")
+let n1,n2,n3
+n1=n2=n3=2+2
+let gc=233
+gc++
+console.log(gc)
